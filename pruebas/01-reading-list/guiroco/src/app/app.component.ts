f69373954daccbb0a4f093af1ffb817e76f50f86
Willy <romerocolomer@gmail.com>
2023-07-13 20:10:44 +0200

    testing husky

--- pruebas/01-reading-list/guiroco/src/app/app.component.ts
+++ pruebas/01-reading-list/guiroco/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	title = "guiroco";
-  aa = "asdasd";
+	title = 'guiroco';
+	aa = 'asdasd';
 }
